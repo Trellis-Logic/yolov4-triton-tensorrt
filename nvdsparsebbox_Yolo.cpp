03366d9a0797d7073aaea9485382b31d267e2860
Dan Walkes <danwalkes@trellis-logic.com>
2022-07-27 13:16:58 -0600

    Modify tensor format to match expected
    With this build I no longer crash during run with deepstream/triton
    inference server.  However I am not seeing bounding boxes displayed
    properly, most likely because I'm missing the NMS step.

--- nvdsparsebbox_Yolo.cpp
+++ nvdsparsebbox_Yolo.cpp
@@ -29,114 +29,27 @@
 #include <unordered_map>
 #include "nvdsinfer_custom_impl.h"
 
-static const int NUM_CLASSES_YOLO = 80;
+#define MIN(a,b) ((a) < (b) ? (a) : (b))
+#define MAX(a,b) ((a) > (b) ? (a) : (b))
+#define CLIP(a,min,max) (MAX(MIN(a, max), min))
 
-float clamp(const float val, const float minVal, const float maxVal)
-{
-    assert(minVal <= maxVal);
-    return std::min(maxVal, std::max(minVal, val));
-}
+ // See https://github.com/NVIDIA-AI-IOT/deepstream_tao_apps/blob/master/post_processor/nvdsinfer_custombboxparser_tao.cpp#L77-L78
 
-extern "C" bool NvDsInferParseCustomYoloV4(
-    std::vector<NvDsInferLayerInfo> const& outputLayersInfo,
-    NvDsInferNetworkInfo const& networkInfo,
-    NvDsInferParseDetectionParams const& detectionParams,
-    std::vector<NvDsInferParseObjectInfo>& objectList);
-
-static NvDsInferParseObjectInfo convertBBoxYoloV4(const float& bx1, const float& by1, const float& bx2,
-                                     const float& by2, const uint& netW, const uint& netH)
-{
-    NvDsInferParseObjectInfo b;
-    // Restore coordinates to network input resolution
-
-    float x1 = bx1 * netW;
-    float y1 = by1 * netH;
-    float x2 = bx2 * netW;
-    float y2 = by2 * netH;
-
-    x1 = clamp(x1, 0, netW);
-    y1 = clamp(y1, 0, netH);
-    x2 = clamp(x2, 0, netW);
-    y2 = clamp(y2, 0, netH);
-
-    b.left = x1;
-    b.width = clamp(x2 - x1, 0, netW);
-    b.top = y1;
-    b.height = clamp(y2 - y1, 0, netH);
-
-    return b;
-}
-
-static void addBBoxProposalYoloV4(const float bx, const float by, const float bw, const float bh,
-                     const uint& netW, const uint& netH, const int maxIndex,
-                     const float maxProb, std::vector<NvDsInferParseObjectInfo>& binfo)
-{
-    NvDsInferParseObjectInfo bbi = convertBBoxYoloV4(bx, by, bw, bh, netW, netH);
-    if (bbi.width < 1 || bbi.height < 1) return;
-
-    bbi.detectionConfidence = maxProb;
-    bbi.classId = maxIndex;
-    binfo.push_back(bbi);
-}
-
-static std::vector<NvDsInferParseObjectInfo>
-decodeYoloV4Tensor(
-    const float* boxes, const float* scores,
-    const uint num_bboxes, NvDsInferParseDetectionParams const& detectionParams,
-    const uint& netW, const uint& netH)
-{
-    std::vector<NvDsInferParseObjectInfo> binfo;
-
-    uint bbox_location = 0;
-    uint score_location = 0;
-    for (uint b = 0; b < num_bboxes; ++b)
-    {
-        float bx1 = boxes[bbox_location];
-        float by1 = boxes[bbox_location + 1];
-        float bx2 = boxes[bbox_location + 2];
-        float by2 = boxes[bbox_location + 3];
-
-        float maxProb = 0.0f;
-        int maxIndex = -1;
-
-        for (uint c = 0; c < detectionParams.numClassesConfigured; ++c)
-        {
-            float prob = scores[score_location + c];
-            if (prob > maxProb)
-            {
-                maxProb = prob;
-                maxIndex = c;
-            }
-        }
-
-        if (maxProb > detectionParams.perClassPreclusterThreshold[maxIndex])
-        {
-            addBBoxProposalYoloV4(bx1, by1, bx2, by2, netW, netH, maxIndex, maxProb, binfo);
-        }
-
-        bbox_location += 4;
-        score_location += detectionParams.numClassesConfigured;
-    }
-
-    return binfo;
-}
-
-
-/* C-linkage to prevent name-mangling */
-
-static bool NvDsInferParseYoloV4(
+static bool NvDsInferParseCustomNMSTLT(
     std::vector<NvDsInferLayerInfo> const& outputLayersInfo,
     NvDsInferNetworkInfo const& networkInfo,
     NvDsInferParseDetectionParams const& detectionParams,
     std::vector<NvDsInferParseObjectInfo>& objectList)
 {
-    if (NUM_CLASSES_YOLO != detectionParams.numClassesConfigured)
-    {
-        std::cerr << "WARNING: Num classes mismatch. Configured:"
-                  << detectionParams.numClassesConfigured
-                  << ", detected by network: " << NUM_CLASSES_YOLO << std::endl;
-    }
 
+    float* out_nms = (float *) outputLayersInfo[0].buffer;
+    const int out_class_size = detectionParams.numClassesConfigured;
+    const float threshold = detectionParams.perClassThreshold[0];
+    // 7 colums per row in the format [x, y, w, h, box_confidence, class_id, class_prob]
+    // See example at https://github.com/isarsoft/yolov4-triton-tensorrt/blob/543fde846b2751d6ab394339e005e2754de22972/clients/python/processing.py#L76
+    const int num_rows = outputLayersInfo[0].inferDims.numElements / 7;
+
+#if 0
     std::cout << "Parser output layers" << std::endl;
 
     for (auto it = outputLayersInfo.begin();
@@ -152,40 +65,41 @@ static bool NvDsInferParseYoloV4(
             std::cout << "    Dim " << dim << " size " << info.inferDims.d[dim] << std::endl;
         }
     }
-
     std::cout << "End of output layers" << std::endl;
+#endif
 
-    std::vector<NvDsInferParseObjectInfo> objects;
-    const NvDsInferLayerInfo &boxes = outputLayersInfo[0]; // num_boxes x 4
-    const NvDsInferLayerInfo &scores = outputLayersInfo[1]; // num_boxes x num_classes
-    const NvDsInferLayerInfo &subbox = outputLayersInfo[2];
+    for (int i = 0; i < num_rows; i++) {
+        float *det = out_nms + i * 7;
+        float box_confidence = det[4];
+        float class_confidence = det[6];
+        float confidence = box_confidence * class_confidence;
+        int class_id = det[5];
+        float x = det[0];
+        float y = det[1];
+        float w = det[2];
+        float h = det[3];
 
-    //* printf("%d\n", subbox.inferDims.numDims);
-    // 3 dimensional: [num_boxes, 1, 4]
-    assert(boxes.inferDims.numDims == 3);
-    // 2 dimensional: [num_boxes, num_classes]
-    if (scores.inferDims.numDims != 2) {
-        std::cerr << "WARNING: expected 2 dims in scores output, found "
-                  << scores.inferDims.numDims
-                  << std::endl;
+        // Output format for each detection is stored in the below order
+        if ( confidence < threshold ) continue;
+        std::cout << "adding box with confidence " << confidence << " box,class " << box_confidence
+          << "," << class_confidence << " greater than threshold " << threshold << std::endl;
+
+        std::cout << "x " << x << " y " << y << " w " << w << " h " << h << " class_id " << class_id << " box,class " << box_confidence
+          << class_confidence << " greater than threshold " << threshold << std::endl;
+
+        assert( class_id < out_class_size );
+        NvDsInferObjectDetectionInfo object;
+            object.classId = class_id;
+            object.detectionConfidence = confidence;
+
+            /* Clip object box co-ordinates to network resolution */
+            object.left = CLIP(x * networkInfo.width, 0, networkInfo.width - 1);
+            object.top = CLIP(y * networkInfo.height, 0, networkInfo.height - 1);
+            object.width = CLIP(w * networkInfo.width, 0, networkInfo.width - 1);
+            object.height = CLIP(h * networkInfo.height, 0, networkInfo.height - 1);
+
+            objectList.push_back(object);
     }
-    if (scores.inferDims.numDims >= 2) {
-        // The second dimension should be num_classes
-        assert(detectionParams.numClassesConfigured == scores.inferDims.d[1]);
-    }
-
-    uint num_bboxes = boxes.inferDims.d[0];
-
-    // std::cout << "Network Info: " << networkInfo.height << "  " << networkInfo.width << std::endl;
-
-    std::vector<NvDsInferParseObjectInfo> outObjs =
-        decodeYoloV4Tensor(
-            (const float*)(boxes.buffer), (const float*)(scores.buffer), num_bboxes, detectionParams,
-            networkInfo.width, networkInfo.height);
-
-    objects.insert(objects.end(), outObjs.begin(), outObjs.end());
-
-    objectList = objects;
 
     return true;
 }
@@ -196,7 +110,7 @@ extern "C" bool NvDsInferParseCustomYoloV4(
     NvDsInferParseDetectionParams const& detectionParams,
     std::vector<NvDsInferParseObjectInfo>& objectList)
 {
-    return NvDsInferParseYoloV4 (
+    return NvDsInferParseCustomNMSTLT (
         outputLayersInfo, networkInfo, detectionParams, objectList);
 }
 
